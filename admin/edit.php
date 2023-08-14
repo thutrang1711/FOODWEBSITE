1cbfd54ba5332bc12d844c72c954235ed0bcda54
damnhan1109 <133198577+damnhan1109@users.noreply.github.com>
2023-08-14 13:54:34 +0700

    edit.php

---
+++ admin/edit.php
@@ -0,0 +1,8 @@
+require_once('database/dbhelper.php');
+            <a class="nav-link" href="category/index.php">Quản lý danh mục</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="product/">Quản lý sản phẩm</a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link active" href="dashboard.php">Quản lý giỏ hàng</a>
